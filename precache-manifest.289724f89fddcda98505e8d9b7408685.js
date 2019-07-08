f040250ee73cd42f92b7bdeeecfa7cab7a3241ef
TheKaizenWolf <raul670saavedra@gmail.com>
2019-07-08 08:09:09 -0400

    Updates

---
+++ precache-manifest.289724f89fddcda98505e8d9b7408685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324fd0e3d9bc78cbe1cb25e956675f91",
+    "url": "/roboamigos/index.html"
+  },
+  {
+    "revision": "7a7a4db5285fba2fc5ff",
+    "url": "/roboamigos/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bde0ffe04d92954f1c7e",
+    "url": "/roboamigos/static/css/main.1930c42e.chunk.css"
+  },
+  {
+    "revision": "7a7a4db5285fba2fc5ff",
+    "url": "/roboamigos/static/js/2.abe2c6b4.chunk.js"
+  },
+  {
+    "revision": "bde0ffe04d92954f1c7e",
+    "url": "/roboamigos/static/js/main.4991ff50.chunk.js"
+  },
+  {
+    "revision": "b06944eb663b283a8f81",
+    "url": "/roboamigos/static/js/runtime~main.cd6f200e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboamigos/static/media/SEGA.12a3db8b.woff"
+  }
+]);
