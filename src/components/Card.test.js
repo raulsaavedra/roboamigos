ea7b598da2a6e3dfe73b983143271058fcf3a681
TheKaizenWolf <raul670saavedra@gmail.com>
2019-08-13 16:29:19 -0400

    tsx

--- src/components/Card.test.js
+++
@@ -1,9 +0,0 @@
-import { shallowToJson  } from 'enzyme-to-json';
-import {shallow} from 'enzyme';
-import React from 'react';
-import Card from './Card';
-
-it('expect to render card component', () => {
-	const card = shallow(<Card />);
-	expect(shallowToJson(card)).toMatchSnapshot();
-})
