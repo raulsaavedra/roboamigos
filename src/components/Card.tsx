d6e4b238fbf8f8e9cd0fdf9a43268841f9481b42
TheKaizenWolf <raul670saavedra@gmail.com>
2019-10-16 14:50:31 -0300

    update formatting

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
 
 interface CardStateLessProps {
-	name: string,
-	email: string,
-	id: number
+  name: string;
+  email: string;
+  id: number;
 }
 
-const Card: React.SFC<CardStateLessProps> = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
+const Card: React.SFC<CardStateLessProps> = ({ name, email, id }) => (
+  <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+    <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+    <div>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,24 +1,18 @@
 import * as React from 'react';
 import Card from './Card';
-import {IRobot} from '../containers/App';
+import { IRobot } from '../containers/App';
 
-const CardList = ({ robots } : {robots: Array<IRobot>}) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
-}
+const CardList = ({ robots }: { robots: Array<IRobot> }) => (
+  <div>
+    {robots.map((user, i) => (
+      <Card
+        key={i}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+      />
+    ))}
+  </div>
+);
 
 export default CardList;
